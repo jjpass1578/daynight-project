99c46c385bf4c5b0fb84e25583731ad6cf1e0f5c
jjpass1578 <2qt2bstr87888@gmail.com>
2016-03-03 01:17:05 -0500

    daynight-project
    almost completed day/night project

---
+++ daynight.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('#orb').click(function(){
+        if ($(this).hasClass('sun')) {
+            $(this).removeClass('sun').addClass('moon');
+            $('#sky').removeClass('day').addClass('night');
+            
+        }
+        else {
+            $(this).removeClass('moon').addClass('sun');
+            $('#sky').removeClass('night').addClass('day');
+        }
+        
+    
+    });
+});
